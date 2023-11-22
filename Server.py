a69596a66c2504870c0b7575d5ac47892deec71d
Viktor Krivetskiy <129954045+m1kk1mouse@users.noreply.github.com>
2023-11-22 13:41:35 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-#pyftpdlib docs https://pyftpdlib.readthedocs.io/en/latest/index.html
+# pyftpdlib docs https://pyftpdlib.readthedocs.io/en/latest/index.html
 
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
@@ -19,17 +19,24 @@ DIRECTORY = "/home/q/ftp"
 
 def main():
     authorizer = DummyAuthorizer()
-    authorizer.add_user(USER, PASS, DIRECTORY, perm='w')    #Perm set permission, w - only write
+    authorizer.add_user(USER, PASS, DIRECTORY, perm='emwlr')    
+        # perm - set permission:
+        # "e" = change directory (CWD, CDUP commands)
+        # "m" = create directory (MKD command)
+        # "w" = store a file to the server (STOR, STOU commands)
+        # "l" = list files (LIST, NLST, STAT, MLSD, MLST, SIZE commands)
+        # "r" = retrieve file from the server (RETR command)
 
     handler = FTPHandler
     handler.log_prefix = 'XXX [%(username)s]@%(remote_ip)s' #Adds extended logs
     handler.authorizer = authorizer
+    handler.banner = "Welcome to my FTP server"
 
     address = (ADDRESS, PORT)
     server = FTPServer(address, handler)
 
     server.max_cons = 100   #Maximum number of server connections
-    server.max_cons_per_ip = 1  #Maximum number of server connections from a single IP
+    server.max_cons_per_ip = 5  #Maximum number of server connections from a single IP
     handler.max_login_attempts = 2  #Maximum number of sign-in attempts
 
     server.serve_forever()  #Prevents the server from shutting down automatically
